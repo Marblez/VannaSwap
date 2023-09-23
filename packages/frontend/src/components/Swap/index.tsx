434031c0b0506f99576578a84759a829e873c5ed
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-09-23 13:39:11 -0400

    add swap

--- packages/frontend/src/components/Swap/index.tsx
+++ packages/frontend/src/components/Swap/index.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useState } from 'react'
 import styled from 'styled-components'
 import { MdSettings } from 'react-icons/md';
 import { BsArrowDownShort, BsArrowUpShort } from 'react-icons/bs';
+import Image from 'next/image';
 
 type Props = {}
 const WidgetContainer = styled.div`
@@ -75,41 +76,98 @@ const TextContainer = styled.div`
     font-size: 14px;
     color: rgb(155, 155, 155);
 `
-const SwapButton = styled.button`
-    background-color: rgb(34, 211, 238);
-    font-size: 20px;
-    font-weight: 535;
-    padding: 16px;
-    width: 100%;
-    line-height: 24px;
-    font-weight: 535;
-    text-align: center;
-    border-radius: 16px;
-    outline: none;
-    border: 1px solid transparent;
-    color: rgb(255, 255, 255);
-    text-decoration: none;
+const InputContainer = styled.div`
     display: flex;
-    -webkit-box-pack: center;
-    justify-content: center;
-    flex-wrap: nowrap;
+    flex-flow: row;
     -webkit-box-align: center;
     align-items: center;
-    cursor: pointer;
-    position: relative;
-    z-index: 1;
-    will-change: transform;
-    transition: transform 450ms ease 0s;
-    transform: perspective(1px) translateZ(0px);
-    :disabled {
-        background-color: #ffffff11 ;
-        color: #9b9b9b;
-        cursor: auto;
-        box-shadow: none;
-        outline: none;
-    }
+    -webkit-box-pack: justify;
+    justify-content: space-between;
 `
+const StyledInput = styled.input`
+    filter: none;
+    opacity: 1;
+    transition: opacity 250ms ease-in-out 0s;
+    text-align: left;
+    font-size: 36px;
+    font-weight: 485;
+    max-height: 44px;
+    color: rgb(255, 255, 255);
+    pointer-events: auto;
+    width: 0px;
+    position: relative;
+    font-weight: 485;
+    outline: none;
+    border: none;
+    flex: 1 1 auto;
+    background-color: transparent;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    padding: 0px;
+    appearance: textfield;
+    text-align: left;
+`
+const TokenContainer = styled.div`
+    display: inline-block;
+    height: inherit;
+`
+const TokenBox = styled.span`
+    display: flex;
+    align-items: center;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    border-radius: 16px;
+    background-color: rgb(19, 19, 19);
+    color: rgb(255, 255, 255);
+    padding: 4px 8px 4px 4px;
 
+`
+const IconBox = styled.div`
+    width: 24px;
+    height: 24px;
+    background: none;
+    transition: background-color 250ms ease-in 0s;
+    box-shadow: white 0px 0px 1px;
+    border-radius: 50%;
+    margin-right: 4px;
+`
+const NumberContainer = styled.div`
+    -webkit-box-pack: end;
+    justify-content: flex-end;
+    min-height: 24px;
+    padding: 8px 0px 0px;
+`
+const NumberBox = styled.div`
+    width: 100%;
+    display: flex;
+    padding: 0px;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+`
+const BalanceBox = styled.div`
+    height: 16px;
+    width: fit-content;
+    display: flex;
+    padding: 0px;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: start;
+    justify-content: flex-start;
+`
+const MaxButton = styled.button`
+    background-color: transparent;
+    border: none;
+    color: #22d3ee;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: 535;
+    opacity: 1;
+    padding: 4px 6px;
+    pointer-events: initial;
+`
 const Swap = (props: Props) => {
     const [disabled, setDisabled] = useState(false)
     const toggleButton = useCallback(() => {
@@ -132,6 +190,36 @@ const Swap = (props: Props) => {
                             <TextContainer>
                                 You pay
                             </TextContainer>
+                            <InputContainer>
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexGrow: 1,
+                                    }}>
+                                    <StyledInput placeholder="0" />
+                                </div>
+                                <div>
+                                    <TokenContainer>
+                                        <TokenBox>
+                                            <IconBox>
+                                                <Image src='/eth-logo.png' width={24} height={24} alt='eth-logo' />
+                                            </IconBox>
+                                            <div className='text-[20px]'>
+                                                ETH
+                                            </div>
+                                        </TokenBox>
+                                    </TokenContainer>
+                                </div>
+                            </InputContainer>
+                            <NumberContainer>
+                                <NumberBox>
+                                    <div></div>
+                                    <BalanceBox>
+                                        <div>Balance: 3.13</div>
+                                        <MaxButton>Max</MaxButton>
+                                    </BalanceBox>
+                                </NumberBox>
+                            </NumberContainer>
                         </SwapInnerContainer>
                     </SwapContainer>
                     <SwitchContainer>
@@ -145,6 +233,35 @@ const Swap = (props: Props) => {
                                 <TextContainer>
                                     You receive
                                 </TextContainer>
+                                <InputContainer>
+                                    <div
+                                        style={{
+                                            display: 'flex',
+                                            flexGrow: 1,
+                                        }}>
+                                        <StyledInput placeholder="0" />
+                                    </div>
+                                    <div>
+                                        <TokenContainer>
+                                            <TokenBox>
+                                                <IconBox>
+                                                    <Image src='/usdc-logo.png' width={24} height={24} alt='eth-logo' />
+                                                </IconBox>
+                                                <div className='text-[20px]'>
+                                                    USDC
+                                                </div>
+                                            </TokenBox>
+                                        </TokenContainer>
+                                    </div>
+                                </InputContainer>
+                                <NumberContainer>
+                                    <NumberBox>
+                                        <div></div>
+                                        <BalanceBox>
+                                            <div>Balance: 2,772.3</div>
+                                        </BalanceBox>
+                                    </NumberBox>
+                                </NumberContainer>
                             </SwapInnerContainer>
                         </SwapContainer>
                     </div>
