9f062da42eacd6fc286add92132e1afd225c824b
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-09-24 02:28:16 -0400

    add volatility

--- packages/frontend/src/components/Pools/index.tsx
+++ packages/frontend/src/components/Pools/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Image from 'next/image'
 import dayjs from 'dayjs'
 import { BsArrowDownShort, BsArrowUpShort } from 'react-icons/bs';
-
+import { ethers } from 'ethers'
+import { gql, useClient, useQuery } from 'urql';
+import { std as calculateStandardDeviation } from 'mathjs'
 type Props = {}
 const PoolsContainer = styled.div`
     box-sizing: border-box;
@@ -33,7 +35,61 @@ const Badge = styled.div`
     padding: 4px 6px;
     font-weight: 400;
 `
+const query = gql`
+  query($blockNumber:Int!){
+    pool(id: "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8",block:{ number: $blockNumber }) {
+        tick
+        token0Price
+        token1Price
+      }
+    }
+
+`
 const Pools = (props: Props) => {
+    const client = useClient()
+    const [blockNumber, setBlockNumber] = useState(0)
+    const [volatilityArray, setVolatilityArray] = useState([])
+    const fetchBlockNumber = async () => {
+        const provider = new ethers.JsonRpcProvider('https://docs-demo.quiknode.pro/')
+        const _blockNumber = await provider.getBlockNumber()
+        setBlockNumber(_blockNumber)
+    }
+    const getVolatility = async (block: number) => {
+        const queryPromise = []
+        for (let currentBlock = block; currentBlock > block - 300; currentBlock = currentBlock - 15) {
+            console.log('currentBlock', currentBlock)
+            const loadData = client.query(query, { blockNumber: currentBlock }).toPromise();
+            queryPromise.push(loadData)
+        }
+        const data = await Promise.all(queryPromise)
+        const priceData = data.map((item: any) => {
+            return Number(item.data.pool.token0Price)
+        })
+        //calculate volatility for 5 days ,10 days and 20 days
+        const volatilityArray = []
+        console.log(priceData.slice(0, 5))
+        console.log(calculateStandardDeviation(priceData.slice(0, 5)))
+        volatilityArray.push(calculateStandardDeviation(priceData.slice(0, 5)))
+        volatilityArray.push(calculateStandardDeviation(priceData.slice(0, 10)))
+        volatilityArray.push(calculateStandardDeviation(priceData.slice(0, 20)))
+
+        console.log(priceData)
+        console.log(data)
+        console.log(volatilityArray)
+        //@ts-ignore
+        setVolatilityArray([...volatilityArray])
+
+
+    }
+    useEffect(() => {
+        fetchBlockNumber()
+    }, [])
+    useEffect(() => {
+        if (blockNumber) {
+            getVolatility(blockNumber)
+        }
+    }, [blockNumber])
+    console.log('blockNumber', blockNumber)
     return (
         <PoolsContainer>
             <div className="grid grid-cols-6 gap-4">
@@ -60,7 +116,9 @@ const Pools = (props: Props) => {
                     </div>
                 </div>
                 <div className='flex justify-end'>$225.37m</div>
-                <div className='flex justify-end'>10% / 13.7% / 9.9%</div>
+                <div className='flex justify-end'>
+                    {volatilityArray.length && `${(volatilityArray[0] * 100).toFixed(2)}% / ${(volatilityArray[1] * 100).toFixed(2)}% / ${(volatilityArray[2] * 100).toFixed(2)}%`}
+                </div>
                 <div className='flex justify-end items-center gap-2'>
                     <div className='flex items-center text-green-300'>
                         <BsArrowUpShort size={20} />
