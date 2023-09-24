8a9b5536f2c524798fd1304f1f540aa04d2cdae4
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-09-24 06:57:11 -0400

    update

--- packages/scripts/constant.js
+++ packages/scripts/constant.js
@@ -84,4 +84,4 @@ export const VOLATILITY_ORACLE_ABI = [
         "type": "function"
     }
 ]
-export const VOLATILITY_ORACLE_ADDRESS = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
+export const VOLATILITY_ORACLE_ADDRESS = '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9'
